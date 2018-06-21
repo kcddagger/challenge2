3ca025435164da4f59faeecd859af8fab52c2e02
Crystal Buckneberg <crb@kcddagger.com>
2018-06-20 23:34:40 -0500

    challenge2 final code

--- challenge2/Program.cs
+++ challenge2/Program.cs
@@ -10,8 +10,33 @@ namespace challenge2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            string n;
+
+            Console.WriteLine("Enter the duration of your ride in minutes. It is guaranteed you have been riding less than a day (24 hours).");
+            n = Console.ReadLine();
+
+            lateRide(n);
+        }
+
+        private static void lateRide(string n)
+        {
+            DateTime rideStart = new DateTime();
+            int m;
+            int.TryParse(n, out m);
+            TimeSpan rideLength = new TimeSpan(00, m, 00);
+            DateTime currentTime = new DateTime();
+
+            currentTime = rideStart.Add(rideLength);
+            string strCurrentTime = String.Format("{0:HHmmss}", currentTime);
+            int timeTotal = 0;
+
+            foreach (char c in strCurrentTime)
+            {
+                timeTotal += int.Parse(c.ToString());
+            }
+
+            Console.WriteLine(timeTotal);
+            Console.ReadLine();
         }
     }
 }
